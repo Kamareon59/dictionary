6730021025b6a7fac8adb1df4a73e6fef69e541a
Kamareon59 <93917055+Kamareon59@users.noreply.github.com>
2022-06-06 17:01:24 +0200

    Initial set-up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Plain and simple</h1>
+      <p>A humble dictionary</p>
+    </div>
+  );
+}
+
+export default App;
