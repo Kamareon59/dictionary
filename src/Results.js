7d1b5c5b33a35e24622b7d0ec211f837ea3c3651
Kamareon59 <93917055+Kamareon59@users.noreply.github.com>
2022-06-08 23:32:22 +0200

    Add general synonyms to results
    Instead of only specific synonyms related to specific definitions

--- src/Results.js
+++ src/Results.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Meanings from "./Meanings";
 import "./Results.css";
+import Synonyms from "./Synonyms";
 
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
         <h2 className="text-capitalize">{props.results.word}</h2>
+        <div>
+          <Synonyms synonyms={props.results.meanings[0].synonyms} />
+        </div>
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,11 +1,10 @@
 import React from "react";
+import "./Synonyms.css";
 
 export default function Synonyms(props) {
-  console.log(props.synonyms);
   if (props.synonyms.length > 0) {
     return (
       <div className="Synonyms">
-        Synonyms:
         <ul>
           {props.synonyms.map(function (synonym, index) {
             return <li key={index}>{synonym}</li>;
