c6918e072305bee11b1c4681eca489a630cd1e8e
Kamareon59 <93917055+Kamareon59@users.noreply.github.com>
2022-06-09 00:00:36 +0200

    Add phonetic text and audio to results

--- src/Results.js
+++ src/Results.js
@@ -1,13 +1,21 @@
 import React from "react";
+import Phonetics from "./Phonetics";
 import Meanings from "./Meanings";
-import "./Results.css";
 import Synonyms from "./Synonyms";
+import "./Results.css";
 
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
-        <h2 className="text-capitalize">{props.results.word}</h2>
+        <h2>{props.results.word}</h2>
+        {props.results.phonetics.map(function (phonetic, index) {
+          return (
+            <div key={index} className="mb-3">
+              <Phonetics phonetic={phonetic} />
+            </div>
+          );
+        })}
         <div>
           <Synonyms synonyms={props.results.meanings[0].synonyms} />
         </div>
